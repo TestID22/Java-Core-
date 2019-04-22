f11190861aff6b2eeb1d7e2ad3b1877a362da40e
September <vitaliiseptember@gmail.com>
2019-04-22 09:49:06 +0300

    First Prog

--- JavaLearn/src/com/company/Main.java
+++ JavaLearn/src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello, GitHub! Its my first prog in Java! Today:");
+        System.out.println(new Date());
 	// write your code here
     }
 }
