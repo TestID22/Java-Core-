dd5b22c6ef2e39410ffa92c0ec35686df431a114
September <vitaliiseptember@gmail.com>
2019-04-29 15:19:37 +0300

    cycle For example

---
+++ cycle/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int x, y;
+	    x = 0;
+	    y = 20;
+	    int num = 100;
+	    for (x=0; x < 10; x++) { // фигурные скобки обозначают блочную систему
+			System.out.println("Текущее значение x: " + x);
+			System.out.println("Текущее значение y" + y);
+			System.out.println(num);
+			num = num - 1;
+			y = y - 2;
+		}
+    }
+}
