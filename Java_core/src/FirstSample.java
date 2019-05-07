53c0b079c103580397d01b761042d9541975e23d
September <vitaliiseptember@gmail.com>
2019-05-07 13:16:10 +0300

    del_files

--- Java_core/src/FirstSample.java
+++ Java_core/src/FirstSample.java
@@ -1,5 +1,37 @@
-public class FirstSample {
+import java.util.*;
+
+public class FirstSample{
     public static void main(String[] args){
-        System.out.println("HI!");
+        //lesson_ 1 Java Core
+        String hi = "Hello";
+        System.out.println(hi);
+        int random = 1;
+        for (random = 0; random < 10; random ++ )
+            System.out.println(random + 1);
+
+        // Константы
+        final int NUMBER_DAYS = 7;
+        System.out.println(NUMBER_DAYS + ":Количество дней в неделе");
+
+        // Инкремент
+        int num_increment = 1;
+        num_increment ++;
+        System.out.println("Число в переменной увеличилось на 1, такого в Питоне нет" +num_increment);
+
+        int n = 5, m = 10;
+        int a = n + ++m;
+        int b = n + m++;
+        System.out.println(a);
+        System.out.println(b);
+
+        // Подстроки или Срез Питона
+        String hello = "Hello";
+        String h = hello.substring(0, 4);
+        System.out.println(h);
+
+        // Метод equals Проверка на идентичность строк
+        String stroke_e = "Hello";
+        if (stroke_e.equals(hello))
+            System.out.println("Строки равны");
     }
 }
