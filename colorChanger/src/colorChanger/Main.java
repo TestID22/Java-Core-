f963c9bf44fda70decfab163afc9967ab88b057a
septemberEatMe <vitaliiseptember@gmail.com>
2019-06-30 10:38:22 +0300

    AnonClasses change ColorPanel

---
+++ colorChanger/src/colorChanger/Main.java
@@ -0,0 +1,46 @@
+package colorChanger;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	CreateFrame frame = new CreateFrame();
+	JPanel panel = new JPanel();
+	JButton bt = new JButton("BLACK");
+	bt.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			panel.setBackground(Color.CYAN);
+			
+		}
+	});
+	panel.add(bt);
+	frame.add(panel);
+	
+	}
+}
+
+
+
+class CreateFrame extends JFrame{
+	public CreateFrame() {
+		setTitle("Main window");
+		setSize(300, 300);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+}
+
+
+	
+
