14e1704c51eaab449f0c4b350f429f9d6ac0c22b
septemberEatMe <vitaliiseptember@gmail.com>
2019-07-01 17:42:24 +0300

    interface ActionListerner for JButton

---
+++ buttonListenerTest/src/buttonListenerTest/Main.java
@@ -0,0 +1,49 @@
+package buttonListenerTest;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = getMeFrame("Main");
+		JPanel panel = new JPanel();
+		JButton bt = new JButton("Make This Window BLACK");
+		JButton btRed = new JButton("Сделай красиво");
+		panel.add(bt);
+		panel.add(btRed);
+		frame.add(panel);
+		btRed.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel.setBackground(Color.RED);
+				
+			}
+		});
+		
+		bt.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel.setBackground(Color.BLACK);
+				
+			}
+		});
+		
+	
+	}
+		static JFrame getMeFrame(String title) {
+			JFrame frame = new JFrame();
+			frame.setTitle(title);
+			frame.setBounds(400,200,400,400);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+			return frame;
+		}
+}
