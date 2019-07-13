56aa4b5685c8aeffecede89be9084a82db8880e0
septemberEatMe <vitaliiseptember@gmail.com>
2019-07-13 12:54:52 +0300

    add comments genericz

--- genericsTest/src/com/september/generics/Main.java
+++ genericsTest/src/com/september/generics/Main.java
@@ -1,7 +1,11 @@
 package com.september.generics;
-
-
 import java.util.*;
+
+/**
+ * @author Vitalii September
+ * В Джавке 5 использовали Массивы и приведение типов
+ * С дженериками стало легче тперь тип приводится автоматически
+ */
 
 public class Main {
     public static void main(String[] args) {
@@ -15,17 +19,20 @@ public class Main {
         String name = (String) interMilan.get(0); //каждый раз необходимо использовать приведение типа
 
         System.out.println(name);
+        System.out.println();
 
         //=========
         List <String> forzaInter = new ArrayList<>();
         forzaInter.add("Nicola Barella");
         //Треню триксы из питона
         String [] stalkers = "Pazza Inter Amala".split(" ");
-
         for (String s : stalkers)
-            forzaInter.add(s);
+            forzaInter.add(s); // из массива сталкерс добавим в forzaInter
+
         String nameF = forzaInter.get(0);
-        System.out.println(nameF);
+        System.out.println(nameF + " Этот элемент хранится в forzainter.get(0)");
+        System.out.println();
+
         for (String s: forzaInter)
             System.out.println(s);
         //reverse string Аеее without StringBuilder and
