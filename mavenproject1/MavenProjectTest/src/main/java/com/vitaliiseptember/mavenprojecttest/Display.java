a2941353c1b7049254e6fa1d48b545c8a2f4f880
septemberEatMe <vitaliiseptember@gmail.com>
2019-08-12 13:29:34 +0300

    Game project

---
+++ mavenproject1/MavenProjectTest/src/main/java/com/vitaliiseptember/mavenprojecttest/Display.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vitaliiseptember.mavenprojecttest;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author vital
+ */
+public class Display extends JFrame{
+    private JFrame frame;
+    private static Canvas canvas;
+    Hero hero = new Hero();
+
+    public Display() {
+        frame = new JFrame("window");
+        frame.setVisible(true);
+        frame.setSize(1000,700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(hero);
+        
+        canvas = new Canvas(){
+            @Override
+            public void paint(Graphics g) {
+                super.paint(g); //To change body of generated methods, choose Tools | Templates.
+                render(g);
+            }
+            
+        };
+        canvas.setBackground(Color.BLACK);
+        frame.add(canvas);
+        frame.getContentPane().add(canvas);
+        
+    }
+    public static void render(){
+        canvas.repaint();
+    }
+    public static void render(Graphics g){
+        g.setColor(Color.white);
+        g.fillRect(20, 20, 30, 20);
+        
+        
+    }
+}
